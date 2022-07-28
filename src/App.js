c3286ed27bc9285cf9ccf57203770329785b62be
alrud5 <alexander.rudberg@inter.ikea.com>
2022-07-28 08:41:40 +0200

    ADD - first component and prop

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
+import Header from "./components/Header"
+
 function App() {
     return (
-        <div className='container'>
-            <h1>My App</h1>
-        </div>
+        <>
+            <Header />
+            <div className='container'>
+                <h1>My App</h1>
+            </div>
+        </>
     )
-}
+};
 
 export default App
